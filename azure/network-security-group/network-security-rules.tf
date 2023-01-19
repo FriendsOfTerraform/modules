019b00fd6bb2fd9f366c3feeb3097091c845bf41
Peter Sin <psin@sgb-connect.com>
2023-01-19 21:12:41 +0000

    updated nsg module to support terraform v1.3

--- azure/network-security-group/network-security-rules.tf
+++ azure/network-security-group/network-security-rules.tf
@@ -5,7 +5,7 @@ resource "azurerm_network_security_rule" "inbound_security_rules" {
   resource_group_name                        = data.azurerm_resource_group.current.name
   network_security_group_name                = azurerm_network_security_group.network_security_group.name
   description                                = each.value.description
-  protocol                                   = each.value.protocol != null ? each.value.protocol : "Tcp" # default protocol to TCP
+  protocol                                   = each.value.protocol
   source_port_range                          = "*"
   destination_port_range                     = each.value.port_ranges != null ? null : "*" # allow all port ranges if not specified
   destination_port_ranges                    = each.value.port_ranges != null ? each.value.port_ranges : null
@@ -13,7 +13,7 @@ resource "azurerm_network_security_rule" "inbound_security_rules" {
   source_application_security_group_ids      = each.value.source_application_security_group_ids
   destination_address_prefixes               = each.value.destination_ip_addresses
   destination_application_security_group_ids = each.value.destination_application_security_group_ids
-  access                                     = each.value.action != null ? each.value.action : "Allow" # default to Allow if not specified
+  access                                     = each.value.action
   priority                                   = each.value.priority
   direction                                  = "Inbound"
 
@@ -39,7 +39,7 @@ resource "azurerm_network_security_rule" "outbound_security_rules" {
   resource_group_name                        = data.azurerm_resource_group.current.name
   network_security_group_name                = azurerm_network_security_group.network_security_group.name
   description                                = each.value.description
-  protocol                                   = each.value.protocol != null ? each.value.protocol : "Tcp" # default protocol to TCP
+  protocol                                   = each.value.protocol
   source_port_range                          = "*"
   destination_port_range                     = each.value.port_ranges != null ? null : "*" # allow all port ranges if not specified
   destination_port_ranges                    = each.value.port_ranges != null ? each.value.port_ranges : null
@@ -47,7 +47,7 @@ resource "azurerm_network_security_rule" "outbound_security_rules" {
   source_application_security_group_ids      = each.value.source_application_security_group_ids
   destination_address_prefixes               = each.value.destination_ip_addresses
   destination_application_security_group_ids = each.value.destination_application_security_group_ids
-  access                                     = each.value.action != null ? each.value.action : "Allow" # default to Allow if not specified
+  access                                     = each.value.action
   priority                                   = each.value.priority
   direction                                  = "Outbound"
 
