aad7c4b84caaccb4b2afdd821321b5920f692146
petersin0422 <peter.sin.0422@gmail.com>
2023-07-26 11:19:44 -0700

    added ability to restrict access to CIDR ranges when using private only endpoint

---
+++ aws/eks/security-group.tf
@@ -0,0 +1,29 @@
+data "aws_subnet" "control_plane_subnet" {
+  id = var.vpc_config.subnet_ids[0]
+}
+
+# This is only needed for private only endpoint. To control access from particular CIDRs
+resource "aws_security_group" "control_plane_security_group" {
+  count = var.enable_apiserver_public_endpoint ? 0 : 1
+
+  name                   = "${var.name}-control-plane-security-group"
+  description            = "Allow API access to the ${var.name} cluster"
+  vpc_id                 = data.aws_subnet.control_plane_subnet.vpc_id
+  revoke_rules_on_delete = true
+
+  tags = merge(
+    local.common_tags,
+    var.additional_tags_all
+  )
+}
+
+resource "aws_security_group_rule" "allow_https" {
+  count = var.enable_apiserver_public_endpoint ? 0 : 1
+
+  type              = "ingress"
+  from_port         = "443"
+  to_port           = "443"
+  protocol          = "tcp"
+  cidr_blocks       = var.apiserver_allowed_cidrs
+  security_group_id = aws_security_group.control_plane_security_group.id
+}
