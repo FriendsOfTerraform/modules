96face6bf18c727781dce62ed24fff32412453a5
Peter Sin <peter.sin.0422@gmail.com>
2025-10-08 19:42:42 -0700

    base commit

---
+++ aws/sagemaker-inference/sagemaker-model.tf
@@ -0,0 +1,62 @@
+resource "aws_sagemaker_model" "models" {
+  for_each = var.models
+
+  name                     = each.key
+  execution_role_arn       = each.value.iam_role_arn
+  enable_network_isolation = each.value.enable_network_isolation
+
+  dynamic "container" {
+    for_each = each.value.container_definitions
+
+    content {
+      image              = container.value.image
+      mode               = container.value.use_multiple_models != null ? "MultiModdel" : "SingleModel"
+      model_data_url     = lower(container.value.compression_type) == "compressedmodel" ? container.value.model_data_location : null
+      container_hostname = container.key
+      environment        = container.value.environment_variables
+
+      dynamic "model_data_source" {
+        for_each = lower(container.value.compression_type) == "uncompressedmodel" ? [1] : []
+
+        content {
+          s3_data_source {
+            compression_type = endswith(container.value.model_data_location, ".tar.gz") ? "Gzip" : "None"
+            s3_data_type     = endswith(container.value.model_data_location, "/") ? "S3Prefix" : "S3Object"
+            s3_uri           = container.value.model_data_location
+          }
+        }
+      }
+
+      dynamic "multi_model_config" {
+        for_each = container.value.use_multiple_models != null ? [1] : []
+
+        content {
+          model_cache_setting = container.value.use_multiple_models.enable_model_caching ? "Enabled" : "Disabled"
+        }
+      }
+    }
+  }
+
+  dynamic "inference_execution_config" {
+    for_each = length(each.value.container_definitions) > 1 ? [1] : []
+
+    content {
+      mode = each.value.inference_execution_config.mode
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = each.value.vpc_config != null ? [1] : []
+
+    content {
+      security_group_ids = each.value.vpc_config.security_group_ids
+      subnets            = each.value.vpc_config.subnet_ids
+    }
+  }
+
+  tags = merge(
+    local.common_tags,
+    each.value.additional_tags,
+    var.additional_tags_all
+  )
+}
---
+++ aws/sagemaker-inference/variables.tf
@@ -0,0 +1,98 @@
+variable "models" {
+  type = map(object({
+    iam_role_arn = string
+
+    container_definitions = map(object({
+      image                 = string
+      compression_type      = optional(string, "CompressedModel")
+      environment_variables = optional(map(string), {})
+      model_data_location   = optional(string, null)
+
+      use_multiple_models = optional(object({
+        enable_model_caching = optional(bool, true)
+      }), null)
+    }))
+
+    inference_execution_config = optional(object({
+      mode = optional(string, "Serial")
+    }), {})
+
+    additional_tags          = optional(map(string), {})
+    enable_network_isolation = optional(bool, false)
+
+    vpc_config = optional(object({
+      security_group_ids = list(string)
+      subnet_ids         = list(string)
+    }), null)
+  }))
+  description = ""
+}
+
+variable "additional_tags_all" {
+  type        = map(string)
+  description = "Additional tags for all resources deployed with this module"
+  default     = {}
+}
+
+variable "endpoints" {
+  type = map(object({
+    additional_tags = optional(map(string), {})
+    encryption_key  = optional(string, null)
+
+    provisioned = optional(object({
+      production_variants = map(object({
+        instance_type               = string
+        container_startup_timeout   = optional(string, null)
+        initial_instance_count      = optional(number, 1)
+        initial_weight              = optional(number, 1)
+        model_data_download_timeout = optional(string, null)
+        volume_size                 = optional(number, null)
+      }))
+
+      async_invocation_config = optional(object({
+        s3_output_path                          = string
+        encryption_key                          = optional(string, null)
+        error_notification_location             = optional(string, null)
+        include_response_in_topics              = optional(string, "None")
+        max_concurrent_invocations_per_instance = optional(number, null)
+        s3_failure_path                         = optional(string, null)
+        success_notification_location           = optional(string, null)
+      }), null)
+
+      enable_data_capture = optional(object({
+        s3_location_to_store_data_collected = string
+        sampling_percentage                 = optional(number, 30)
+
+        capture_content_type = optional(object({
+          csv_text = optional(list(string), null)
+          json     = optional(list(string), null)
+        }), null)
+
+        data_capture_options = optional(object({
+          prediction_request  = optional(bool, true)
+          prediction_response = optional(bool, true)
+        }), {})
+      }), null)
+
+      shadow_variants = optional(map(object({
+        instance_type               = string
+        container_startup_timeout   = optional(number, null)
+        initial_instance_count      = optional(number, 1)
+        initial_weight              = optional(number, 1)
+        model_data_download_timeout = optional(number, null)
+        volume_size                 = optional(number, null)
+      })), {})
+    }), null)
+
+    serverless = optional(object({
+      variant = object({
+        model_name              = string
+        max_concurrency         = optional(number, 20)
+        memory_size             = optional(number, 1024)
+        provisioned_concurrency = optional(number, null)
+      })
+    }), null)
+  }))
+  description = ""
+  default     = {}
+}
