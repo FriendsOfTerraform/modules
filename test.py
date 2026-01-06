ff5fc8b555a8fb2d3b79c63d1b79ef6b5fa9ea04
Vladimir Jimenez <allejo@me.com>
2026-01-05 23:36:05 -0800

    feat: replace top-level variable descriptions

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import re
 folder = 'aws/elastic-load-balancer'
 varFile = ''
 
-docRe = r'- \([\w()]+\) \*\*`([^`\s]+)(?:\s=.+)?`\*\*\s_\[since\sv([\w\d.]+)\]_\n+(\s*)(.*)'
+docRe = r'([ ]*)- \([\w()]+\) \*\*`([^`\s]+)(?:\s=.+)?`\*\*\s_\[since\sv([\w\d.]+)\]_\n+(\s*)(.*)'
 linkRefRe = r'\[([^\]]+)\]\[([^\]]+)\]'
 linkRefDefRe = r'\[([^\]]+)\]:\s*(.*)'
 
@@ -16,21 +16,42 @@ with open(f'{folder}/README.md', 'r') as f:
   linkRefDictMatches = re.findall(linkRefDefRe, lines)
   linkRefDict = { m[0]: m[1] for m in linkRefDictMatches }
 
-  for match in variables:
-    docBlkSrc = [match[3], '']
+  for idx, match in enumerate(variables):
+    indent = len(match[0]) // 2
+    pathName = '.'.join([m[1] for m in variables[max(0, idx-indent):min(len(variables)-1, idx+1)]])
+    varName = match[1]
+    since = match[2]
+    description = match[4]
 
-    linkRefs = re.findall(linkRefRe, match[3])
-
+    linkRefs = re.findall(linkRefRe, description)
     if linkRefs:
       pass
 
-    docBlk = f"""
-/// {match[3]}
-///
-/// @since {match[1]}
-{match[0]} =
-""".rstrip()
+    docBlkSrc = [description, '', f'@since {since}']
+    renderDocBlk = lambda prefix: '\n'.join([f'{prefix}{l}' for l in docBlkSrc])
 
-    varFile = re.sub(rf'\s+{match[0]}\s+=', docBlk, varFile)
+    if indent == 0:
+      varDefRe = r'(variable "' + re.escape(varName) + r'"[\s\S]*?description = )([^\n]*)'
+      varFileSplit = re.search(varDefRe, varFile, re.MULTILINE)
+      descReplacement = f"""<<EOT
+{renderDocBlk('    ')}
+  EOT"""
+
+      if varFileSplit is None:
+        print(f'Variable definition not found for top-level variable: {varName}')
+        continue
+
+      varFile = f'{varFile[0:varFileSplit.end(1)]}{descReplacement}{varFile[varFileSplit.end(2):]}'
+    else:
+      pass
+
+#     docBlk = f"""
+# /// {description}
+# ///
+# /// @since {since}
+# {varName} =
+# """.rstrip()
+
+#     varFile = re.sub(rf'\s+{varName}\s+=', docBlk, varFile)
 
   print(varFile)
