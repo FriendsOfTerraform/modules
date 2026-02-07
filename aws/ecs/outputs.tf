07e08719a03f26a57aad4f3bd3cfb981eaa8aac3
Vladimir Jimenez <allejo@me.com>
2026-02-06 19:31:12 -0800

    docs(aws/ecs): migrate outputs to new format

--- aws/ecs/outputs.tf
+++ aws/ecs/outputs.tf
@@ -1,4 +1,10 @@
 output "ec2_capacity_provider_arns" {
+  description = <<EOT
+    Map of ARNs of all EC2 capacity providers
+    
+    @type map(string)
+    @since 1.0.0
+  EOT
   value = {
     for name, provider in var.ec2_capacity_providers :
     name => aws_ecs_capacity_provider.capacity_providers[name].arn
@@ -6,5 +12,11 @@ output "ec2_capacity_provider_arns" {
 }
 
 output "ecs_cluster_arn" {
+  description = <<EOT
+    The ARN of the ECS cluster
+    
+    @type string
+    @since 1.0.0
+  EOT
   value = aws_ecs_cluster.ecs_cluster.arn
 }
