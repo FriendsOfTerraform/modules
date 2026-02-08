7e493d108e1054983f0c0a969e52a12201fa4680
Vladimir Jimenez <allejo@me.com>
2026-02-07 23:27:22 -0800

    docs(aws/roles-anywhere): migrate to new format

--- aws/roles-anywhere/outputs.tf
+++ aws/roles-anywhere/outputs.tf
@@ -1,15 +1,39 @@
 output "profile_arns" {
+  description = <<EOT
+    Map of ARNs of all profiles
+    
+    @type map(string)
+    @since 1.0.0
+  EOT
   value = { for k, v in aws_rolesanywhere_profile.profiles : k => v.arn }
 }
 
 output "profile_ids" {
+  description = <<EOT
+    Map of IDs of all profiles
+    
+    @type map(string)
+    @since 1.0.0
+  EOT
   value = { for k, v in aws_rolesanywhere_profile.profiles : k => v.id }
 }
 
 output "trust_anchor_arns" {
+  description = <<EOT
+    Map of ARNs of all trust anchors
+    
+    @type map(string)
+    @since 1.0.0
+  EOT
   value = { for k, v in aws_rolesanywhere_trust_anchor.trust_anchors : k => v.arn }
 }
 
 output "trust_anchor_ids" {
+  description = <<EOT
+    Map of IDs of all trust anchors
+    
+    @type map(string)
+    @since 1.0.0
+  EOT
   value = { for k, v in aws_rolesanywhere_trust_anchor.trust_anchors : k => v.id }
 }
