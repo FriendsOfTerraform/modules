8b7f712f7fc432637ae4b90ae210cee2880f87d0
Vladimir Jimenez <allejo@me.com>
2026-02-07 23:14:45 -0800

    docs(aws/resource-access-manager): migrate to new format

--- aws/resource-access-manager/variables.tf
+++ aws/resource-access-manager/variables.tf
@@ -1,40 +1,69 @@
 variable "name" {
   type        = string
-  description = "The name of the share. All associated resources' names will also be prefixed by this value"
+  description = <<EOT
+    The name of the resource share. All associated resources will also have their name prefixed with this value
+    
+    @since 1.0.0
+  EOT
 }
 
 variable "accept_sharings" {
   type        = list(string)
-  description = "ARNs of the share to accept"
+  description = <<EOT
+    List of share ARNs to accept sharing from
+    
+    @since 1.0.0
+  EOT
   default     = []
 }
 
 variable "additional_tags" {
   type        = map(string)
-  description = "Additional tags for the share"
+  description = <<EOT
+    Additional tags for the resource share
+    
+    @since 1.0.0
+  EOT
   default     = {}
 }
 
 variable "additional_tags_all" {
   type        = map(string)
-  description = "Additional tags for all resources deployed with this module"
+  description = <<EOT
+    Additional tags for all resources deployed with this module
+    
+    @since 1.0.0
+  EOT
   default     = {}
 }
 
 variable "allow_external_principals" {
   type        = bool
-  description = "whether principals outside your organization can be associated with a resource share"
+  description = <<EOT
+    If enabled, you can share resources with any AWS accounts, roles, and users. If you are in an organization, you can also share with the entire organization or organizational units in that organization.
+    
+    @since 1.0.0
+  EOT
   default     = false
 }
 
 variable "principals" {
   type        = list(string)
-  description = "List of principals to grant access of the resources"
+  description = <<EOT
+    List of principals to grant access of the resources to. Valid values include: `the 12-digits AWS account ID, ARN of an AWS Organization, AWS Organization's OU, IAM role, IAM user, or a Service principal`.
+    
+    @since 1.0.0
+  EOT
   default     = []
 }
 
 variable "resources" {
   type        = list(string)
-  description = "List of ARNs of the resources to share"
+  description = <<EOT
+    List of ARNs of supported resources to share. Please refer to [this documentation][ram-shareable-resources] for a list of shareable resources.
+    
+    @link {ram-shareable-resources} https://docs.aws.amazon.com/ram/latest/userguide/shareable.html
+    @since 1.0.0
+  EOT
   default     = []
 }
